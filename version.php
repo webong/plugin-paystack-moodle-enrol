4d917e4bf144a756ce56848ccad61a56a584392b
wisdomanthoni <wizitendo10@gmail.com>
2019-08-25 23:15:21 +0100

    set plugin release to 1.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_paystack';
-$plugin->release = '0.1.0';
+$plugin->release = '1.0';
 $plugin->version = 2019081900;
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_STABLE;
